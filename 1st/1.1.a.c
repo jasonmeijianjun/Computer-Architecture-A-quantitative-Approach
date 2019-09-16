8f5a39cfe0576f0df11f15f416bb23e8bf9ca5d4
Jason <jianjun.mei@broadcom.com>
2019-09-16 08:23:19 +0530

    Corrected the formula

--- 1st/1.1.a.c
+++ 1st/1.1.a.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-
+#define N 4.0
 int main(void)
 {
 	double chip_area;
@@ -8,7 +8,7 @@ int main(void)
 	double chip_flaw_rate;
 	chip_area = 3.89;
 	chip_flaw_rate = 0.3;
-	good_rate =pow(( 1 - chip_flaw_rate ),chip_area);
+	good_rate =pow(( 1 + chip_flaw_rate * chip_area ),-N);
 
 	printf("Chip good_rate = %f\n",good_rate);
 }
